40a95e03c2bf2a760907ac60cd1ea704def271cb
未来 栗山 <2410128587@qq.com>
2023-01-12 20:41:26 +0800

    update CarouselView.

--- QianShiMusicClient.Maui/ViewModels/MainViewModel.cs
+++ QianShiMusicClient.Maui/ViewModels/MainViewModel.cs
@@ -9,19 +9,52 @@ namespace QianShiMusicClient.Maui.ViewModels;
 public partial class MainViewModel : ViewModelBase
 {
     [ObservableProperty]
+    [NotifyPropertyChangedFor(nameof(CurrentItem))]
     int _currentViewIndex;
 
-    public List<TabBarItem> Tabs { get; } 
+    [System.Diagnostics.CodeAnalysis.SuppressMessage("CommunityToolkit.Mvvm.SourceGenerators.ObservablePropertyGenerator", "MVVMTK0034:Invalid task scheduler exception flow option usage", Justification = "<挂起>")]
+    partial void OnCurrentViewIndexChanged(int value)
+    {
+        var current = Tabs[value];
+        if (current != _currentItem)
+        {
+            _currentItem = current;
+        }
+    }
+
+    [ObservableProperty]
+    FoundViewModel _foundVm;
+
+    [ObservableProperty]
+    HomeViewModel _homeVm;
+
+    [ObservableProperty]
+    [NotifyPropertyChangedFor(nameof(CurrentViewIndex))]
+    TabBarItem? _currentItem;
+
+    [System.Diagnostics.CodeAnalysis.SuppressMessage("CommunityToolkit.Mvvm.SourceGenerators.ObservablePropertyGenerator", "MVVMTK0034:Invalid task scheduler exception flow option usage", Justification = "<挂起>")]
+    partial void OnCurrentItemChanged(TabBarItem? value)
+    {
+        if (value is null)
+        {
+            _currentViewIndex = -1;
+            return;
+        }
+        var index = Tabs.IndexOf(value);
+        _currentViewIndex = index;
+    }
+
+    public List<TabBarItem> Tabs { get; }
 
     public MainViewModel()
     {
-        var foundVm = ServiceHelper.GetRequiredService<FoundViewModel>();
-        var homeVm = ServiceHelper.GetRequiredService<HomeViewModel>();
+        _foundVm = ServiceHelper.GetRequiredService<FoundViewModel>();
+        _homeVm = ServiceHelper.GetRequiredService<HomeViewModel>();
 
         Tabs = new List<TabBarItem>
         {
-            new TabBarItem("发现",IconFontIcons.Faxian, typeof(FoundView), foundVm),
-            new TabBarItem("我的",IconFontIcons.Music, typeof(HomeView),homeVm),
+            new TabBarItem("发现",IconFontIcons.Faxian, typeof(FoundView), _foundVm),
+            new TabBarItem("我的",IconFontIcons.Music, typeof(HomeView),_homeVm),
         };
     }
 }
